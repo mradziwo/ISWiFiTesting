6db262044a7206a05a7ee494d0fcc344383b709a
Mike <mradziwo@users.noreply.github.com>
2018-10-05 21:30:06 +0200

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,43 @@
+import requests
+import msgpack
+import time
+def Get(url):
+    r = requests.get(url, headers={'Expect': 'application/msgpack'}, stream=True, timeout=5)
+    return (r.status_code, msgpack.unpackb(r.content, use_list=False, raw=False))
+
+host = 'http://192.168.4.1:80'
+urls= ['/Time/Time',
+       '/Time/SystemStartTime',
+       '/Time/SystemUptime',
+       '/Runtime/LastIgnition',
+       '/Runtime/StateUptime',
+       '/Runtime/State',
+       '/Errors/Lockout',
+       '/Errors/LastErrorTime',
+       '/Errors/LastError',
+#       '/Errors/LastLockoutReleseTime',
+#       '/Errors/ErrorList',
+       '/Properties/ID',
+       '/Properties/Firmware',
+       '/Properties/Hardware',
+       '/Properties/LastChange',
+       '/Properties/Changes',
+       '/Properties/Log',
+       '/Properties/PermLog',
+       '/Properties/Settings',
+       '/Properties/Settings',
+       '/Properties/UART1',
+       '/Properties/UART2',
+       '/Properties/APIversion',
+       '/Querry/Interval',
+       '/Modbus/Address',
+       '/Modbus/GatewayTimeoutChange',
+       '/ISM010_all',
+       '/ISM010_available',
+       '/FileList']
+for prop in urls:
+    url = host+prop
+    time.sleep(1)
+    print (url)
+    result=Get(url)
+    print (prop,Get(url))
